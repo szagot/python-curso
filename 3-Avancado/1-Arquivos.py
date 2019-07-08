c58c405b4b1ca9d75c1b1d2a9a4589093e32342b
Daniel Bispo <4570461+szagot@users.noreply.github.com>
2019-07-08 18:49:40 -0300

    Update 1-Arquivos.py

--- 3-Avancado/1-Arquivos.py
+++ 3-Avancado/1-Arquivos.py
@@ -5,13 +5,13 @@ Trabalhando com arquivos
 # Mostra em que pasta você está
 pwd
 
-# Criando um arquivo
+# Criando um arquivo (comando do jupyter)
 %%writefile arquivo.ext
 Conteuto
 """
 
 # Abrindo um arquivo no Python
-arquivo = open('arquivo.ext')
+arquivo = open('arquivo.txt')
 
 # Lendo uma linha do arquivo. Após a leitura, o cursor passa para alinha seguinte
 linha = arquivo.readline()
@@ -19,5 +19,10 @@ linha = arquivo.readline()
 # Lendo todo o arquivo a partir de onde está o cursor
 conteudo = arquivo.read()
 
-# Voltando o cursor para o inicio
+# Voltando o cursor para o inicio (o parametro informa o caracter onde o cursor deve parar
 arquivo.seek(0)
+
+# Lendo o arquivo de texto com laço de repeticão
+for linha in arquivo:
+    print(linha)
+
