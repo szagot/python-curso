5eedd4c779a10dbfc826db6cf2646faba6b29393
Daniel Bispo <4570461+szagot@users.noreply.github.com>
2019-07-08 19:03:14 -0300

    Update and rename 6-Listas-Compostas.py to 6-Listas-Compostas-e-Conjuntos.py
    Adicionadas informações sobre conjuntos (set)

--- 2-Básico/6-Listas-Compostas-e-Conjuntos.py
+++ 2-Básico/6-Listas-Compostas-e-Conjuntos.py
@@ -11,6 +11,11 @@ print(dados)
 
 print(dados[0][0])
 #-> Resultado: 'Filipe'
+
+
+Outro tipo de dados são os conjuntos (set). São como as listas, porém só permitem valores únicos
+exemplo = set()
+exemplo2 = set(pessoa1)
 """
 
 # Pegando os dados e salvando em uma lista composta
@@ -28,3 +33,20 @@ for i in range(0, 3):
 # Mostrando os dados de uma lista composta
 for pessoa in pessoas:
     print(f'{pessoa[0]} tem {pessoa[1]} anos de idade')
+
+# Criando um conjunto
+conjunto = set()
+
+# Adicionando dados. O exemplo abaixo retornará {1,2} por que ele não repete valores já adicionados
+conjunto.add(1)
+conjunto.add(2)
+conjunto.add(1)
+
+# Lendo conjunto
+for item in conjunto:
+    print(item)
+
+# É possível criar um conjunto a partir de uma lista. O conjunto resultante ficará ordenado e sem as repetições
+lista = [1,22,33,1,'Daniel',22]
+conjunto = set(lista)
+# -> Resultado {1, 22, 33, 'Daniel'}
